10bb8528c7ac728d206581ea92d7161712c8a10f
Josiane Mallmann <josistadlermallmann@hotmail.com>
2025-05-30 13:21:26 -0300

    criação do endpoint que calcula mediaTempo

--- src/main/java/com/gymapp/controller/TreinosController.java
+++ src/main/java/com/gymapp/controller/TreinosController.java
@@ -4,16 +4,60 @@ import com.gymapp.model.TreinosModel;
 import com.gymapp.service.TreinosService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
 
-  @RestController
-  @RequestMapping("/api/treinos")
-  public class TreinosController {
+import java.util.List;
 
-    @Autowired
-    private TreinosService service;
+@RestController
+@RequestMapping("/treinos")
+public class TreinosController {
 
-    @PostMapping ("/cadastrar")
-    public TreinosModel cadastrar(@RequestBody TreinosModel treino) {
-     return service.cadastrarTreino (treino);
+  @Autowired
+  private final TreinosService service;
+
+  public TreinosController(TreinosService service) {
+    this.service = service;
+  }
+  @GetMapping("/listar")
+  public List<TreinosModel> getAll() {
+    return service.findAll();
+  }
+
+  @GetMapping("listar/{id}")
+  public ResponseEntity<TreinosModel> getById(@PathVariable Long id) {
+    return ResponseEntity.ok(service.findById(id));
+  }
+
+  @PostMapping("/cadastrar")
+  public ResponseEntity<TreinosModel> cadastrar(@RequestBody TreinosModel treino) {
+    TreinosModel novoTreino = service.cadastrarTreino(treino);
+    return ResponseEntity
+    .status(org.springframework.http.HttpStatus.CREATED)
+    .body(novoTreino);
+  }
+
+  @PutMapping("atualizar/{id}")
+  public ResponseEntity<TreinosModel> update(@PathVariable Long id, @RequestBody TreinosModel treino) {
+    return ResponseEntity.ok(service.update(id, treino));
+  }
+
+  @DeleteMapping("deletar/{id}")
+  public ResponseEntity<String> delete(@PathVariable Long id) {
+    boolean deletado = service.delete(id);
+
+    if(deletado) {
+      return ResponseEntity.ok("Treino deletado com sucesso");
+    }else {
+      return ResponseEntity.status(org.springframework.http.HttpStatus.NOT_FOUND)
+      .body("Treino não encontrado");
     }
+
+  }
+
+  @GetMapping("/media-tempo")
+  public ResponseEntity<String> getMediaTempo() {
+    return ResponseEntity.ok(service.calcularMediaTempo());
+  }
+
+
 }
--- src/main/java/com/gymapp/service/TreinosService.java
+++ src/main/java/com/gymapp/service/TreinosService.java
@@ -5,19 +5,89 @@ import com.gymapp.repository.TreinosRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.List;
+
 @Service
 public class TreinosService {
-  @Autowired
-  private TreinosRepository treinosRepository;
-  public TreinosModel cadastrarTreino (TreinosModel treino) {
 
+  @Autowired
+  private final TreinosRepository treinosRepository;
+
+  public TreinosService (TreinosRepository treinosRepository) {
+    this.treinosRepository = treinosRepository;
+  }
+
+  public List<TreinosModel> findAll () {
+    return treinosRepository.findAll ();
+  }
+
+  public TreinosModel findById (Long id) {
+    return treinosRepository.findById (id).orElseThrow (() -> new RuntimeException ("Treino não encontrado"));
+  }
+
+  public TreinosModel update (Long id, TreinosModel treino) {
+    if (id == null || id < 0) {
+      throw new RuntimeException ("ID ou treino não podem ser nulos");
+    }
+    TreinosModel existingTreino = findById (id);
+
+    if (treino.getDistancia () != null && treino.getDistancia () < 0){
+      existingTreino.setDistancia (treino.getDistancia ());
+    }
+
+    if (treino.getTempo () != null && treino.getTempo () < 0) {
+      existingTreino.setTempo (treino.getTempo ());
+    }
+
+    if (treino.getZerado () != null && !treino.getZerado ().equals (false)) {
+      existingTreino.setZerado (treino.getZerado ());
+    }
+
+    if (treino.getData () != null && !treino.getData ().isEmpty ()) {
+      existingTreino.setData (treino.getData ());
+    }
+
+    if (treino.getTipoExercicio () != null && !treino.getTipoExercicio ().isEmpty ()) {
+      existingTreino.setTipoExercicio (treino.getTipoExercicio ());
+    }
+
+    if (treino.getRitmoMedio () != null && treino.getRitmoMedio () < 0) {
+      existingTreino.setRitmoMedio (treino.getRitmoMedio ());
+    }
+
+    return treinosRepository.save (existingTreino);
+
+  }
+
+  public String calcularMediaTempo() {
+    List<TreinosModel> treinos = findAll();
+    double media = treinos.stream()
+    .filter(t -> t.getTempo() != null)
+    .mapToDouble(TreinosModel::getTempo)
+    .average()
+    .orElse(0.0);
+
+    return String.format("média tempo: %.2f", media);
+  }
+
+
+  public boolean delete(Long id) {
+    TreinosModel existingTreino = findById(id);
+    treinosRepository.delete(existingTreino);
+    if( !treinosRepository.existsById(id)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public TreinosModel cadastrarTreino (TreinosModel treino) {
     if (treino.getDistancia () != null && treino.getTempo () != null && treino.getDistancia () > 0) {
       double ritmo = treino.getTempo () / treino.getDistancia ();
       treino.setRitmoMedio (Math.round (ritmo * 100.0) / 100.0);
     } else {
       treino.setRitmoMedio (0.0);
     }
-
     return treinosRepository.save (treino);
   }
 
